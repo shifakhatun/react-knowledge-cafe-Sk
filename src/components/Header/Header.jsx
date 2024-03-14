daa5c057544eb88b95195a076847bd068d653996
shifa <shifa.eee@gmail.com>
2024-03-14 06:35:24 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+    return (
+        <div>
+           <h1 className='text-6xl font-bold'>Knowlwdge-Cafe</h1>
+           <img src="" alt="" />
+        </div>
+    );
+};
+
+export default Header;
